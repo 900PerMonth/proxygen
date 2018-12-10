35da6fcc854a7b0e9956b9665bee21fdbcd49703
DbgErr <34698805+900PerMonth@users.noreply.github.com>
2018-12-10 14:39:20 +0800

    CentOS Linux release 7.4.1708 build.

--- proxygen/deps-centos.sh
+++ proxygen/deps-centos.sh
@@ -30,14 +30,15 @@ cd "$(dirname "$0")"
 
 # RedHet/CentOS
 
-sudo yum -y install autoconf268 automake libtool cmake autoconf-archive gcc-c++ boost-devel
-sudo yum -y install devtoolset-2-gcc-c++ devtoolset-2-binutils-devel
-export CXX=/opt/rh/devtoolset-2/root/usr/bin/g++
+sudo yum -y install autoconf automake libtool cmake autoconf-archive gcc-c++ boost-devel
+sudo yum -y install devtoolset-7-gcc-c++ devtoolset-7-binutils-devel
+export CXX=/opt/rh/devtoolset-7/root/usr/bin/g++
 
 if [ ! -e google-glog ]; then
     echo "fetching glog from github.com"
-    git clone git@github.com:google/glog.git google-glog
+    git clone https://github.com/google/glog.git google-glog
     cd google-glog
+    ./autogen.sh
     ./configure
     make
     sudo make install
@@ -46,9 +47,9 @@ fi
 
 if [ ! -e gflags-gflags ]; then
     echo "fetching gflags from github.com"
-    git clone git@github.com:gflags/gflags gflags-gflags
-    mkdir build && cd build
-    ccmake ..
+    git clone https://github.com/gflags/gflags gflags-gflags
+    cd gflags-gflags
+    ccmake3 . -DBUILD_SHARED_LIBS=ON # emacs embeded shell does not function well
     make
     sudo make install
     cd ..
@@ -58,7 +59,7 @@ if [ ! -e double-conversion ]; then
     echo "Fetching double-conversion from git..."
     git clone https://github.com/floitsch/double-conversion.git double-conversion
     cd double-conversion
-    cmake . -DBUILD_SHARED_LIBS=ON
+    cmake3 . -DBUILD_SHARED_LIBS=ON
     sudo make install
     cd ..
 fi
@@ -74,13 +75,41 @@ git fetch
 git checkout "$folly_rev"
 
 # Build folly
-autoreconf --install
-./configure
+cd ..
+if [ ! -e build ]; then
+    mkdir build
+fi
+cd build
+cmake3 -DBUILD_SHARED_LIBS=ON -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF ..
 make -j$JOBS
 sudo make install
 
 if test $? -ne 0; then
   echo "fatal: folly build failed"
+  exit -1
+fi
+cd ../..
+
+# Get fizz
+if [ ! -e fizz ]; then
+    echo "Fetching fizz from git..."
+    git clone https://github.com/facebookincubator/fizz.git
+fi
+
+# Build fizz
+cd fizz
+git checkout v2018.10.15.00
+
+if [ ! -e build_ ]; then
+    mkdir build_
+fi
+cd build_
+cmake3 -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF ../fizz
+make -j$JOBS
+sudo make install
+
+if test $? -ne 0; then
+  echo "fatal: fizz build failed"
   exit -1
 fi
 cd ../..
@@ -95,7 +124,7 @@ git fetch
 git checkout "$wangle_rev"
 
 # Build wangle
-cmake .
+cmake3 .
 make -j$JOBS
 sudo make install
 
@@ -111,7 +140,7 @@ autoreconf -ivf
 make -j$JOBS
 
 # Run tests
-LD_LIBRARY_PATH=/usr/local/lib make check
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH make check
 
 # Install the libs
 sudo make install
